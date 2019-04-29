90343582439761766dd797d50311ba293937aa65
y-zlong <18710493547@163.com>
2019-04-29 14:20:14 +0800

    pdf模板初始化，创建一个page=5的pdf

--- src/com/github/yzl/Main.java
+++ src/com/github/yzl/Main.java
@@ -1,7 +1,23 @@
 package com.github.yzl;
 
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println();
+	public static void main(String[] args) throws Exception {
+		//创建一个pdf格式的文件
+		PDDocument pdd = new PDDocument();
+		
+		for (int i = 0; i < 5; i++) {
+			PDPage page = new PDPage();
+			pdd.addPage(page);
+		}
+		
+		//将文件保存路径
+		pdd.save("src\\demo.pdf");
+		//关闭
+		pdd.close();
 	}
 }
